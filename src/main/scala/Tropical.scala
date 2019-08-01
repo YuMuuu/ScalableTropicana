50450b500dc8dac359b496403dbdbc52c59f3c51
YuMuuu <c01150788f@edu.teu.ac.jp>
2019-08-02 04:23:10 +0900

    wip

--- src/main/scala/Tropical.scala
+++ src/main/scala/Tropical.scala
@@ -7,20 +7,20 @@ case class T(d: Double) extends Tropical
 case object Infty extends Tropical
 
 trait Semiring[A[_]]{
-  //加法
   val oplus: A => A => A
-  //加法の単位元
   val zero: A
-  //乗法
   val otimes: A => A => A
-  //乗法の単位元
   val one: A
 }
 
 object Semiring {
   implicit val eqTropical: Eq[Tropical] = Eq.fromUniversalEquals
   implicit val showTropical: Show[Tropical] = Show.fromToString
-  implicit val orderTropical: Order[Tropical] = Order.by(???)
+  implicit val orderTropical: Order[Tropical] = Order.fromLessThan {
+    case (_, Infty)     => true
+    case (Infty, _)     => false
+    case (T(xd), T(yd)) => xd <= yd
+  }
 
   implicit def TropicalSemiring: Semiring[Tropical] = new Semiring[Tropical] {
     override val oplus = A => B => min(A, B)
@@ -28,7 +28,7 @@ object Semiring {
     override val otimes: Tropical => Tropical => Tropical = {
       case _ => Infty => Infty
       case Infty => _ => Infty
-      case (T(a: Double): Tropical) => (T(b: Double): Tropical) => (T((a + b): Double): Tropical)
+      case ta:T => tb:T => T(ta.d + tb.d)
     }
     override val one: Tropical = T(0d)
   }
